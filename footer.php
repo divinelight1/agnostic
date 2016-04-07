d79157c9232e30cd4669edf7b1914076194fac0c
divinelight1 <aldinmujkic44@gmail.com>
2016-04-07 14:12:12 +0200

    add aditional info to header and footer

---
+++ footer.php
@@ -0,0 +1,53 @@
+<!-- Footer -->
+
+                    <footer id="footer">
+
+                        <ul class="icons">
+
+                            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+
+                            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+
+                            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+
+                            <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+
+                            <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+
+                        </ul>
+
+                        <ul class="copyright">
+
+                            <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+
+                        </ul>
+
+                    </footer>
+
+
+
+            </div>
+
+
+
+        <!-- Scripts -->
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery.min.js"></script>
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery.scrollex.min.js"></script>
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery.scrolly.min.js"></script>
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/skel.min.js"></script>
+
+            <script src="<?php bloginfo('template_directory'); ?>/assets/js/util.js"></script>
+
+            <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+
+
+
+    </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+
+    <title><?php wp_title(); ?> | <?php bloginfo('name'); ?></title>
+
+        <meta charset="utf-8" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+
+        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/main.css" />
+
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+
+        <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+    <?php wp_head(); ?>
+    </head>
+
+    <body class="landing">
+
+
+
+        <!-- Page Wrapper -->
+
+            <div id="page-wrapper">
+
+
+
+                <!-- Header -->
+
+                    <header id="header" class="alt">
+
+                    <h1><a href="<?php bloginfo('url'); ?>">Agnostic</a></h1>
+
+                        <nav id="nav">
+
+                            <ul>
+
+                                <li class="special">
+
+                                    <a href="#menu" class="menuToggle"><span>Menu</span></a>
+
+                                    <div id="menu">
+
+                                        <ul>
+
+                                            <li><a href="index.html">Home</a></li>
+
+                                            <li><a href="generic.html">Generic</a></li>
+
+                                            <li><a href="elements.html">Elements</a></li>
+
+                                            <li><a href="#">Sign Up</a></li>
+
+                                            <li><a href="#">Log In</a></li>
+
+                                        </ul>
+
+                                    </div>
+
+                                </li>
+
+                            </ul>
+
+                        </nav>
+
+                    </header>
+
+
+
+                <!-- Banner -->
+
+                    <section id="banner">
+
+                        <div class="inner">
+
+                            <h2>Aldin Mujkic</h2>
+
+                            <p>Frontend developer<br />
+
+                            </p>
+
+                            <ul class="actions">
+
+                                <li><a href="#" class="button special">Hire Me</a></li>
+
+                            </ul>
+
+                        </div>
+
+                        <a href="#one" class="more scrolly">See More</a>
+
+                    </section>
+
+>
